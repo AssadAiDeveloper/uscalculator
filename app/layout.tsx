63522e8720ae36c71f714f6f3b33b9f7e0f6a30f
AssadAiDeveloper <s.assad.ye@gmail.com>
2026-06-11 22:22:13 +0200

    feat: add Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,6 +52,17 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+          <head>
+  {/* Google Analytics */}
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-4EJFT7L4VC" />
+  <script dangerouslySetInnerHTML={{__html: `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-4EJFT7L4VC');
+  `}} />
+
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&family=JetBrains+Mono:wght@400;500&display=swap" rel="stylesheet" />
